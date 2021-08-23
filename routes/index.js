5df0ba3cb5219f1ef09360fd43f0dd3fb143efd0
Nayiem <info@hodlercompany.com>
2021-08-23 15:53:12 +0200

    changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
-    title: "Northwest Hosting - Datacenter Solutions",
+    title: "images altfenix",
     routename: "home",
     csrfToken: req.csrfToken()
   });
